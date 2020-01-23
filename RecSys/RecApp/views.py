c3abe66a8614ce5f9669ea154eae38390c6a54ae
Saugatshakya <saugatoff@gmail.com>
2020-01-23 11:27:33 +0545

    Templates and views and urls for recapp

--- RecSys/RecApp/views.py
+++ RecSys/RecApp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Product
+from django.core.paginator import Paginator
+from django.views.generic import (
+    DetailView
+)
 # Create your views here.
+
+def home(request):
+    
+    products = Product.objects.all()
+    paginator = Paginator(products, 20)
+    page = request.GET.get('page')
+
+    products = paginator.get_page(page)
+
+    return render(request, 'home.html',{'products':products})
+
+class Productdetail(DetailView):
+    model = Product
